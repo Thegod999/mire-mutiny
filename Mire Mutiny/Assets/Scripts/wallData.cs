6d369dc5b3323bd72f356d3df3bff2d336598313
LukeIsLak <lukekuligowicz@gmail.com>
2020-04-15 00:37:36 -0700

    Improved Generation
    Had some holes, no problem, now it generates trails to get to those holes, but it is still somewhat glitchy, but I am to tired

---
+++ Mire Mutiny/Assets/Scripts/wallData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallData : MonoBehaviour
+{
+  public int WallCount;
+  void Start() {
+    this.gameObject.SetActive(false);
+  }
+  void OnTriggerEnter2D(Collider other) {
+    if (other.gameObject.tag == "MainCamera") {
+      this.gameObject.SetActive(true);
+    }
+  }
+  void OnTriggerExit2D(Collider other) {
+    if (other.gameObject.tag == "MainCamera") {
+      this.gameObject.SetActive(false);
+    }
+  }
+
+}
