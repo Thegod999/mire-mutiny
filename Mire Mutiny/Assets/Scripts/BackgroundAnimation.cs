bf7ee2d0f7b8b6a538066d47d07b91ebc7323c95
Yod Golshani <yodgolshani@gmail.com>
2020-06-09 16:02:56 -0700

    Began work on menu

---
+++ Mire Mutiny/Assets/Scripts/BackgroundAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimation : MonoBehaviour
+{
+  public Animator anim;
+  public bool ofCourse = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = GetComponent<Animator>();
+      anim.SetBool("ofCourse", ofCourse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      anim.SetBool("ofCourse", ofCourse);
+    }
+}
