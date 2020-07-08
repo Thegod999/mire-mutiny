92af2437a8a4df8a7cc23a42ec43e4aa9d002e25
Yod Golshani <yodgolshani@gmail.com>
2020-07-08 14:07:17 -0700

    Allowed player buff values to be easily edited

--- Mire Mutiny/Assets/Scripts/StartScript.cs
+++ Mire Mutiny/Assets/Scripts/StartScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class StartScript : MonoBehaviour
 {
+  public int literalSpeed;
+  public int shotSpeed;
+  public int shotQuickness;
+  public int shotLife;
+  public int dashTime;
+  public int dashAnimWait;
     // Start is called before the first frame update
     void Start()
     {
-      PlayerPrefs.SetFloat("Shot_Speed", 30);
-      PlayerPrefs.SetFloat("Literal_Speed", 4);
-      PlayerPrefs.SetFloat("Shot_Quickness", 40);
-      PlayerPrefs.SetFloat("Shot_Life", 120);
-      PlayerPrefs.SetFloat("dash_Time", 30);
-      PlayerPrefs.SetFloat("dash_anim_wait", 30);
+      PlayerPrefs.SetFloat("Shot_Speed", shotSpeed); //30
+      PlayerPrefs.SetFloat("Literal_Speed", literalSpeed); //4
+      PlayerPrefs.SetFloat("Shot_Quickness", shotQuickness); //40
+      PlayerPrefs.SetFloat("Shot_Life", shotLife); //120
+      PlayerPrefs.SetFloat("dash_Time", dashTime); //30
+      PlayerPrefs.SetFloat("dash_anim_wait", dashAnimWait); //30
     }
 
     // Update is called once per frame
